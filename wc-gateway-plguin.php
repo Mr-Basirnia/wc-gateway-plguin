f8a6abfcce9ba21a126752ac082068ce9d0944a0
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2022-10-13 12:20:08 +0330

    --feat: create plugin
    Create and set plugin information.

---
+++ wc-gateway-plguin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: ووکامرس پرداخت
+Plugin URI: https://github.com/Mr-Basirnia/wc-gateway-plguin
+Description: پرداخت با پلاگین ووکامرس پرداخت
+Version: 1.0.0
+Author URI: https://github.com/Mr-Basirnia
+*/

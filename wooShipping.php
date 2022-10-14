c37e61f56aad86a99c49a8f231676a7b0a9165db
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2022-10-14 17:14:43 +0330

    --feat: woo shipping info
    * Add description and set enable for default.

--- wooShipping.php
+++ wooShipping.php
@@ -9,8 +9,10 @@ class Woo_Shipping extends WC_Shipping_Method {
 	public function __construct( $instance_id = 0 ) {
 		parent::__construct( $instance_id );
 
-		$this->id    = 'woo_shipping_plugin';
-		$this->title = 'ارسال رایگان به تهران';
+		$this->id                 = 'woo_shipping_plugin';
+		$this->title              = 'ارسال رایگان به تهران';
+		$this->method_description = 'شیوه ارسال به شهرستان تهران';
+		$this->enabled            = 'yes';
 	}
 }
 
